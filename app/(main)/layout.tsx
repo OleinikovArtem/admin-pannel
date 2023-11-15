2b3033e87c2e0d12e856b7e57cafdbf2dbc7796a
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-11-15 20:02:08 +0200

    update main folder

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -21,7 +21,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
