abd0599960450d18d487ec6f1c9cd66f555cdb9e
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-11-22 10:03:39 +0200

    - add Users table and use it

---
+++ app/dashboard/(pages)/users/components/users-table.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+
+import { MdOutlineEdit } from 'react-icons/md'
+
+import { User } from '@/types/user'
+import { Pagination } from '@/types/pagination'
+
+interface UsersTableProps {
+  users?: Pagination<User>
+}
+
+export const UsersTable = ({ users }: UsersTableProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow className="pr-4">
+          <TableHead className="w-[100px]">Image</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Phone</TableHead>
+          <TableHead>Role</TableHead>
+          <TableHead>Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users?.items.map((user) => (
+          <TableRow className="pr-4" key={user.id}>
+            <TableCell>
+              <Image
+                src={user.image || ''}
+                width="0"
+                height="0"
+                sizes="100px"
+                className="w-[84px] h-[84px]"
+                alt={user.name || ''}
+              />
+            </TableCell>
+            <TableCell className="font-medium">{user.name}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+            <TableCell>{user.role}</TableCell>
+            <TableCell>
+              <Link href={'/dashboard/users/' + user.id}>
+                <Button size="icon" variant="ghost" className="hover:text-green-500">
+                  <MdOutlineEdit className="w-5 h-5" />
+                </Button>
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
--- app/dashboard/(pages)/users/page.tsx
+++ app/dashboard/(pages)/users/page.tsx
@@ -1,3 +1,40 @@
+'use client'
+import { usePagination } from '@/hooks/usePagination'
+import { GET_USERS, GET_USERS_TYPE, GET_USERS_VARIABLES_TYPE } from '@/graphql/users/query/getUsers'
+import { useQuery } from '@apollo/client'
+import { useEffect } from 'react'
+import { PaginationPanel } from '@/components/ui/pagination'
+import { UsersTable } from '@/app/dashboard/(pages)/users/components/users-table'
+
 export default function Users() {
-  return <h2>Users</h2>
+  const { pagination, setPagination, goToNextPage, goToPrevPage, goToPage } = usePagination({ totalPages: 0 })
+  const { loading, error, data, refetch } = useQuery<GET_USERS_TYPE, GET_USERS_VARIABLES_TYPE>(GET_USERS, {
+    variables: { page: pagination.currentPage, limit: 4 },
+  })
+
+  useEffect(() => {
+    if (data?.users) {
+      setPagination({
+        ...pagination,
+        totalPages: Number(data.users.totalPages),
+        currentPage: Number(data.users.currentPage),
+      })
+    }
+  }, [data])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  return (
+    <div>
+      <UsersTable users={data?.users} />
+      <PaginationPanel
+        totalPages={data?.users.totalPages || 0}
+        currentPage={data?.users.currentPage || 1}
+        goToNextPage={goToNextPage}
+        goToPrevPage={goToPrevPage}
+        goToPage={goToPage}
+      />
+    </div>
+  )
 }
